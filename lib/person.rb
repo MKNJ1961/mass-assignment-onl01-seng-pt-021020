289572f84dec3699cd4dc99e9bcb767645049ce0
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-14 07:58:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 
-  def intialize(attributes)
+  def intialize()
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
