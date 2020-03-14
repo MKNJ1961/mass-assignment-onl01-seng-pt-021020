d7806fad351f6c623f2c48861295c8d8dc03696f
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-14 08:06:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,24 @@
-# class Person
-#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-#                 :weight, :handed, :complexion, :t_shirt_size,
-#                 :wrist_size, :glove_size, :pant_length, :pant_width
-#
-#
-#   def intialize(attributes)
-#       attributes.each do |key, value|
-#        self.send(("#{key}="), value)
-#      end
-#   end
-# end
-
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each do |key, value|
-      self.send(("#{key}="), value)
-    end
+
+  def intialize(attributes)
+      attributes.each do |key, value|
+       self.send(("#{key}="), value)
+     end
   end
 end
+
+# class Person
+#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+#                 :weight, :handed, :complexion, :t_shirt_size,
+#                 :wrist_size, :glove_size, :pant_length, :pant_width
+# 
+#   def initialize(attributes)
+#     attributes.each do |key, value|
+#       self.send(("#{key}="), value)
+#     end
+#   end
+# end
